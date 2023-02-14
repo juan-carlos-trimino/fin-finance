27543e5e0eedc6e48714fe74121f693398a44da7
Juan Carlos Trimino <juancarlos@trimino.com>
2023-02-14 15:16:18 -0600

    Add more tests.

--- main.go
+++ main.go
@@ -116,7 +116,7 @@ func main() {
 	  time = si.AccurateTime(principal, interest, rate, finances.Annually, finances.Days)  //Years
 	  fmt.Printf("time period = %.4f (Accurate Interest)\n\n", time)
 	  ***/
-
+  /***
   var m finances.Miscellaneous
   var real = (&m).RealInterestRate(0.045, 0.065)
   fmt.Printf("Real Interest Rate = %.2f%%\n", real * 100)
@@ -124,6 +124,20 @@ func main() {
   var ear float64 = m.NominalToEffectiveAnnualRate(0.12, finances.Monthly)
   fmt.Printf("Effective Annual Rate = %.6f%%\n", ear * 100.0)
   fmt.Printf("Nomial Rate = %.6f%%\n", m.EffectiveAnnualToNominalRate(ear, finances.Monthly) * 100.0)
+  ***/
+  var m finances.Mortgage
+  // var payment, totalCost, totalInterest = (&m).CostOfMortgage(300000.00, 2.74 / 100.0, 'm', 15.0, 'y')
+  // fmt.Printf("Payment = $%.2f Total cost = $%.2f Total interest = $%.2f\n", payment, totalCost, totalInterest)
+  // var bir = (&m).MortgageHeloc(200000, 0.065, 100000, 0.105)
+  // fmt.Printf("Blended Interest Rate = %.2f%%\n", bir)
+  var table = m.AmortizationTable(300000.00, 0.03375, 'm', 30.0, 'y')
+  fmt.Printf("payment = $%.2f total cost = $%.2f total interest = $%.2f\n", table.Payment, table.TotalCost, table.TotalInterest)
+  for i, v := range table.Rows {
+    fmt.Printf("pmtNumber = %d payment = $%.2f pmtPrincipal = $%.2f pmtInterest = $%.2f balance = $%.2f\n", i + 1, v.Payment, v.PmtPrincipal, v.PmtInterest, v.Balance)
+    // fmt.Printf("i = %d\n", i)
+  }
+
+
 
 
   // fmt.Println("eps = ", math.Nextafter(1.0, 2.0) - 1.0)
