b8393d9fd6297e886c15f193e4869d4c4fcb19b2
Juan Carlos Trimino <juancarlos@trimino.com>
2023-03-08 11:08:17 -0600

    Add a new function (convertToFloat64).

--- src/webfinances/annuities.go
+++ src/webfinances/annuities.go
@@ -10,30 +10,47 @@ import (
 
 type Annuities struct{}
 
+/***
+To execute this function from a browser:
+annuities/AverageRateOfReturn?ret=5.0&ret=-3.0&ret=12.0&ret=10
+Average Rate of Return = 5.838
+***/
 func (a Annuities) AverageRateOfReturn(res http.ResponseWriter, req *http.Request) () {
   params := req.URL.Query()
   if len(params) != 1 {
-    fmt.Fprintf(res, "Parameters required = 1; parameters provided = %d\n", len(params))
+    fmt.Fprintf(res, "Parameters required = 1; parameters provided = %d", len(params))
     return
   }
-  const length int = 16
-  var returns []float64 = nil
+  //A nil slice is also an empty slice; no allocation of memory.
+  var returns []float64
+  var err = error(nil)
   //Iterate over all the query parameters.
   for _, v := range params {
-    returns = make([]float64, 0, length)
-    for idx := 0; idx < len(v); idx++ {
-      f, err := strconv.ParseFloat(v[idx], 64)
-      if err != nil {
-        fmt.Fprintf(res, "'%s' is not a floating number.\n%s", v[idx], v)
-        return
-      }
-      returns = append(returns, f)
+    returns, err = convertToFloat64(v)
+    if err != nil {
+      fmt.Fprintf(res, "%s", err)
+      return
     }
   }
   var fa finances.Annuities
   var gmr = fa.AverageRateOfReturn(returns) * 100.0
-  fmt.Fprintf(res, "AverageRateOfReturn = %.3f\n", gmr)
+  fmt.Fprintf(res, "Average Rate of Return = %.3f\n", gmr)
 }
 
+func convertToFloat64(values []string) (floats []float64, err error) {
+  var length int = len(values)
+  floats = make([]float64, length, length)
+  var f float64
+  err = error(nil)
+  for idx := 0; idx < length; idx++ {
+    f, err = strconv.ParseFloat(values[idx], 64)
+    if err != nil {
+      err = fmt.Errorf("'%s' is not a floating number.\n%s", values[idx], values)
+      return
+    }
+    floats[idx] = f
+  }
+  return
+}
 
 
