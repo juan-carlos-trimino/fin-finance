2c265536b2449d9672665ac3a364a3097bcd3f9b
Juan Carlos Trimino <juancarlos@trimino.com>
2023-07-18 22:26:39 -0500

    remove lines.

--- src/middlewares/MwCorrelationId.go
+++ src/middlewares/MwCorrelationId.go
@@ -8,17 +8,6 @@ import (
   "time"
 )
 
-/***
-To avoid context keys collisions, a best practice is to create an unexported custom type.
-***/
-//type ctxKey string
-/***
-The correlationIdKey constant is unexported. Hence, there's no risk that another package using the
-same context could override the value that is already set. Even if another package creates the same
-correlationIdKey based on a ctxKey type as well, it will be a different key.
-***/
-const correlationIdKey /*ctxKey*/ string = "correlationIdKey"
-
 func CorrelationId(handler http.HandlerFunc) http.HandlerFunc {
   return func(res http.ResponseWriter, req *http.Request) {
     start := time.Now()
