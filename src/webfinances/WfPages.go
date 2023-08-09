2ca6e01a3dc51147ffe1e8786693d85813d22780
Juan Carlos Trimino <juancarlos@trimino.com>
2023-08-08 22:30:10 -0500

    add actions to the html pages.

--- src/webfinances/WfPages.go
+++ src/webfinances/WfPages.go
@@ -1,13 +1,10 @@
 package webfinances
 
 import (
-//   "finance/finances"
   "finance/misc"
   "fmt"
   "html/template"
   "net/http"
-//   "strconv"
-//   "strings"
 )
 
 var m = misc.Misc{}
@@ -15,6 +12,10 @@ var tmpl *template.Template
 
 func init() {
   fmt.Printf("%s - Entering init/webfinances.\n", m.DTF())
+  /***
+  The Must function wraps around the ParseGlob function that returns a pointer to a template and an
+  error, and it panics if the error is not nil.
+  ***/
   tmpl = template.Must(template.ParseGlob("webfinances/templates/*.html"))
 }
 
@@ -22,7 +23,7 @@ type WfPages struct{}
 
 func (p WfPages) HomePage(res http.ResponseWriter, req *http.Request) {
   fmt.Printf("%s - Entering HomePage/webfinances.\n", m.DTF())
-  tmpl.ExecuteTemplate(res, "index.html", struct {
+  tmpl.ExecuteTemplate(res, "home_page", struct {
     Header string
     Datetime string
   } { "Investments", m.DTF() })
@@ -30,7 +31,7 @@ func (p WfPages) HomePage(res http.ResponseWriter, req *http.Request) {
 
 func (p WfPages) ContactPage(res http.ResponseWriter, req *http.Request) {
   fmt.Printf("%s - Entering ContactPage/webfinances.\n", m.DTF())
-  tmpl.ExecuteTemplate(res, "contact.html", struct {
+  tmpl.ExecuteTemplate(res, "contact_page", struct {
     Header string
     Datetime string
   } { "Investments", m.DTF() })
@@ -38,7 +39,11 @@ func (p WfPages) ContactPage(res http.ResponseWriter, req *http.Request) {
 
 func (p WfPages) AboutPage(res http.ResponseWriter, req *http.Request) {
   fmt.Printf("%s - Entering AboutPage/webfinances.\n", m.DTF())
-  tmpl.ExecuteTemplate(res, "about.html", struct {
+  /***
+  Executing the template means that we take the content from the template files, combine it with
+  data from another source, and generate the final HTML content.
+  ***/
+  tmpl.ExecuteTemplate(res, "about_page", struct {
     Header string
     Datetime string
   } { "Investments", m.DTF() })
@@ -51,7 +56,7 @@ func (p WfPages) PublicHomeFile(res http.ResponseWriter, req *http.Request) {
 
 func (p WfPages) FinancesPage(res http.ResponseWriter, req *http.Request) {
   fmt.Printf("%s - Entering FinancesPage/webfinances.\n", m.DTF())
-  tmpl.ExecuteTemplate(res, "finances.html", struct {
+  tmpl.ExecuteTemplate(res, "finances_page", struct {
     Header string
     Datetime string
   } { "Finances", m.DTF() })
@@ -59,7 +64,7 @@ func (p WfPages) FinancesPage(res http.ResponseWriter, req *http.Request) {
 
 func (p WfPages) SimpleInterestPage(res http.ResponseWriter, req *http.Request) {
   fmt.Printf("%s - Entering SimpleInterestPage/webfinances.\n", m.DTF())
-  tmpl.ExecuteTemplate(res, "simpleinterest.html", struct {
+  tmpl.ExecuteTemplate(res, "simple_interest_page", struct {
     Header string
     Datetime string
   } { "Simple Interest", m.DTF() })
