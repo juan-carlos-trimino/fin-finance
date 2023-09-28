c0841773eee0156d7e61d2c7854535aa742a7d5b
Juan Carlos Trimino <juancarlos@trimino.com>
2023-09-27 22:42:47 -0500

    add annuity due with compounding periods.

--- src/main.go
+++ src/main.go
@@ -154,6 +154,7 @@ func main() {
   }
   fmt.Printf("%s - Using SHUTDOWN_TIMEOUT: %d\n", m.DTF(), SHUTDOWN_TIMEOUT)
   var wfpages = webfinances.WfPages{}
+  var wfadcp = webfinances.NewWfAdCpPages()
   var wfadfv = webfinances.NewWfAdFvPages()
   var wfadpv = webfinances.NewWfAdPvPages()
   var wfoainterest = webfinances.NewWfOaInterestRatePages()
@@ -209,6 +210,7 @@ func main() {
   h.mux["/fin/ordinaryannuity/ga"] = wfoaga.OaGaPages
   h.mux["/fin/ordinaryannuity/perpetuity"] = wfoaperpetuity.OaPerpetuityPages
   h.mux["/fin/annuitydue"] = wfpages.AnnuityDuePage
+  h.mux["/fin/annuitydue/cp"] = wfadcp.AdCpPages
   h.mux["/fin/annuitydue/fv"] = wfadfv.AdFvPages
   h.mux["/fin/annuitydue/pv"] = wfadpv.AdPvPages
   h.mux["/finances"] = wfpages.FinancesPage
