cb301c720711fe5fdedea73cd9be274b910111fb
Juan Carlos Trimino <juancarlos@trimino.com>
2023-10-12 09:29:01 -0500

    set cookies flags.

--- src/sessions/session.go
+++ src/sessions/session.go
@@ -96,35 +96,16 @@ func GetNewUuid() string {
 }
 
 func CreateCookie(sessionToken string) (cookie *http.Cookie) {
+  //https://en.wikipedia.org/wiki/HTTP_cookie
+  //https://httpwg.org/specs/rfc6265.html
   cookie = &http.Cookie{
     Name: "session_token",
     Value: sessionToken,
-    /***
-    It indicates the path that must exist in the requested URL for the browser to send the Cookie
-    header. The forward slash (/) character is interpreted as a directory separator, and
-    subdirectories are matched as well. For example, for Path=/docs:
-    * The request paths /docs, /docs/, /docs/Web/, and /docs/Web/HTTP will all match.
-    * The request paths /, /docsets, /fr/docs will not match.
-    ***/
     Path: "/",
-    //Expires: sessions[sessionToken].Expiry,
-    /***
-    An http-only cookie cannot be accessed by client-side APIs, such as JavaScript. This
-    restriction eliminates the threat of cookie theft via Cross-Site Scripting (XSS). However, the
-    cookie remains vulnerable to Cross-Site Tracing (XST) and Cross-Site Request Forgery (CSRF)
-    attacks.
-    ***/
+    // Expires: sessions[sessionToken].Expiry,
     HttpOnly: true,
-    /***
-    The attribute SameSite can have a value of Strict, Lax or None. With attribute SameSite=Strict,
-    the browsers would only send cookies to a target domain that is the same as the origin domain.
-    This would effectively mitigate Cross-Site Request Forgery (CSRF) attacks. With SameSite=Lax,
-    browsers would send cookies with requests to a target domain even it is different from the
-    origin domain, but only for safe requests such as GET (POST is unsafe) and not third-party
-    cookies (inside iframe). Attribute SameSite=None would allow third-party (cross-site) cookies,
-    however, most browsers require secure attribute on SameSite=None cookies.
-    ***/
     SameSite: http.SameSiteStrictMode,
+    Secure: false,
   }
   return
 }
@@ -135,14 +116,10 @@ func DeleteSession(sessionToken string) (cookie *http.Cookie) {
     Name: "session_token",
     Value: "",
     Path: "/",
-    // Expires: time.Now(),
-    /***
-    It indicates the number of seconds until the cookie expires. A zero or negative number will
-    expire the cookie immediately. If both Expires and Max-Age are set, Max-Age has precedence.
-    ***/
     MaxAge: -1,
     HttpOnly: true,
     SameSite: http.SameSiteStrictMode,
+    Secure: false,
   }
   return
 }
