832caf05ef1a56d4273b9172f238d0c721c616cf
Juan Carlos Trimino <juancarlos@trimino.com>
2023-12-07 20:06:59 -0600

    initial import.

---
+++ src/webfinances/public/js/AdCompoundingPeriods.js
@@ -0,0 +1,46 @@
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  let params = getParams();
+  disableElements(params.cb);
+  setFocus(params.cb);
+});
+
+function setFocus(eid) {
+  // var audio = new Audio('https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3');
+  let tb;
+  if (eid === "lhs-button1") {
+    // audio.play();
+    tb = document.getElementById("fd1-interest");
+  } else if (eid === "lhs-button2") {
+    tb = document.getElementById("fd2-interest");
+  } else if (eid === "lhs-button3") {
+    tb = document.getElementById("fd3-interest");
+  }
+  tb.focus();
+  /***
+  input type="number" doesn't support setSelectionRange.
+  You can use type="text" and inputmode="numeric". This will show a numeric keyboard for
+  mobile users and supports setSelectionRange.
+  ***/
+  // tb.type = "text";
+  tb.setSelectionRange(0, tb.value.length);
+  // tb.type = "number";
+  // console.log(`Position start: ${tb.selectionStart}`);
+  // console.log(`Position end: ${tb.selectionEnd}`);
+}
+
+function disableElements(eid) {
+  if (eid === "lhs-button1") {
+    document.getElementById("lhs-button1").disabled = true;
+    document.getElementById("lhs-button2").disabled = false;
+    document.getElementById("lhs-button3").disabled = false;
+  } else if (eid === "lhs-button2") {
+    document.getElementById("lhs-button1").disabled = true;
+    document.getElementById("lhs-button2").disabled = true;
+    document.getElementById("lhs-button3").disabled = false;
+  } else if (eid === "lhs-button3") {
+    document.getElementById("lhs-button1").disabled = true;
+    document.getElementById("lhs-button2").disabled = false;
+    document.getElementById("lhs-button3").disabled = true;
+  }
+}
