56902a1af4c22eea9b6ed4afb94856eb8255594d
Juan Carlos Trimino <juancarlos@trimino.com>
2024-01-08 22:07:07 -0600

    Add two functions for files and dirs.

--- src/misc/misc.go
+++ src/misc/misc.go
@@ -1,6 +1,7 @@
 package misc
 
 import (
+  "os"
   "time"
 )
 
@@ -13,3 +14,26 @@ func (m Misc) DTF() string {
   return time.Now().UTC().Format(time.RFC3339Nano)
 }
 
+func CheckFileExists(fileName string) (bool, error) {
+  info, err := os.Stat(fileName)
+  if err != nil {
+    if os.IsNotExist(err) {
+      return false, nil
+    } else {
+      return false, err
+    }
+  }
+  return !info.IsDir(), nil
+}
+
+func CheckDirExists(dirName string) (bool, error) {
+  info, err := os.Stat(dirName)
+  if err != nil {
+    if os.IsNotExist(err) {
+      return false, nil
+    } else {
+      return false, err
+    }
+  }
+  return info.IsDir(), nil
+}
