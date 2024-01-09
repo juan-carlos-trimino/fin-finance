f9fc4c56d9ca1e6a962a0fbf960377b7603ff5e2
Juan Carlos Trimino <juancarlos@trimino.com>
2024-01-08 22:13:50 -0600

    add files to save status of each screen.

--- src/webfinances/WfMortgagePages.go
+++ src/webfinances/WfMortgagePages.go
@@ -2,12 +2,15 @@ package webfinances
 
 import (
 	"context"
+	"encoding/json"
 	"finance/finances"
 	"finance/middlewares"
+	"finance/misc"
 	"finance/sessions"
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -38,7 +41,8 @@ func (mp WfMortgagePages) MortgagePages(res http.ResponseWriter, req *http.Reque
     "Entering MortgagePages/webfinances.",
   })
   if req.Method == http.MethodPost || req.Method == http.MethodGet {
-    mf := getMortgageFields(sessions.GetUserName(sessionToken))
+    userName := sessions.GetUserName(sessionToken)
+    mf := getMortgageFields(userName)
     /***
     The functions in Request that allow to extract data from the URL and/or the body revolve around
     the Form, PostForm, and MultipartForm fields; the data are in the form of key-value pairs.
@@ -56,40 +60,40 @@ func (mp WfMortgagePages) MortgagePages(res http.ResponseWriter, req *http.Reque
     the PostForm field instead of the Form field.
     ***/
     if ui := req.FormValue("compute"); ui != "" {  //Values from form and URL.
-      mf.currentPage = ui
+      mf.CurrentPage = ui
     }
     //
-    if strings.EqualFold(mf.currentPage, "rhs-ui1") {
-      mf.currentButton = "lhs-button1"
+    if strings.EqualFold(mf.CurrentPage, "rhs-ui1") {
+      mf.CurrentButton = "lhs-button1"
       if req.Method == http.MethodPost {
-        mf.fd1N = req.PostFormValue("fd1-n")
-        mf.fd1TimePeriod = req.PostFormValue("fd1-tp")
-        mf.fd1Interest = req.PostFormValue("fd1-interest")
-        mf.fd1Compound = req.PostFormValue("fd1-compound")
-        mf.fd1Amount = req.PostFormValue("fd1-amount")
+        mf.Fd1N = req.PostFormValue("fd1-n")
+        mf.Fd1TimePeriod = req.PostFormValue("fd1-tp")
+        mf.Fd1Interest = req.PostFormValue("fd1-interest")
+        mf.Fd1Compound = req.PostFormValue("fd1-compound")
+        mf.Fd1Amount = req.PostFormValue("fd1-amount")
         var n float64
         var i float64
         var amount float64
         var err error
-        mf.fd1Result[1] = ""
-        mf.fd1Result[2] = ""
-        if n, err = strconv.ParseFloat(mf.fd1N, 64); err != nil {
-          mf.fd1Result[0] = fmt.Sprintf("Error: %s -- %+v", mf.fd1N, err)
-        } else if i, err = strconv.ParseFloat(mf.fd1Interest, 64); err != nil {
-          mf.fd1Result[0] = fmt.Sprintf("Error: %s -- %+v", mf.fd1Interest, err)
-        } else if amount, err = strconv.ParseFloat(mf.fd1Amount, 64); err != nil {
-          mf.fd1Result[0] = fmt.Sprintf("Error: %s -- %+v", mf.fd1Amount, err)
+        mf.Fd1Result[1] = ""
+        mf.Fd1Result[2] = ""
+        if n, err = strconv.ParseFloat(mf.Fd1N, 64); err != nil {
+          mf.Fd1Result[0] = fmt.Sprintf("Error: %s -- %+v", mf.Fd1N, err)
+        } else if i, err = strconv.ParseFloat(mf.Fd1Interest, 64); err != nil {
+          mf.Fd1Result[0] = fmt.Sprintf("Error: %s -- %+v", mf.Fd1Interest, err)
+        } else if amount, err = strconv.ParseFloat(mf.Fd1Amount, 64); err != nil {
+          mf.Fd1Result[0] = fmt.Sprintf("Error: %s -- %+v", mf.Fd1Amount, err)
         } else {
           var m finances.Mortgage
           payment, totalCost, totalInterest := m.CostOfMortgage(amount, i / 100.0,
-            mf.fd1Compound[0], n, mf.fd1TimePeriod[0])
-          mf.fd1Result[0] = fmt.Sprintf("Payment: $%.2f", payment)
-          mf.fd1Result[1] = fmt.Sprintf("Total Interest: $%.2f", totalInterest)
-          mf.fd1Result[2] = fmt.Sprintf("Total Cost: $%.2f", totalCost)
+            mf.Fd1Compound[0], n, mf.Fd1TimePeriod[0])
+          mf.Fd1Result[0] = fmt.Sprintf("Payment: $%.2f", payment)
+          mf.Fd1Result[1] = fmt.Sprintf("Total Interest: $%.2f", totalInterest)
+          mf.Fd1Result[2] = fmt.Sprintf("Total Cost: $%.2f", totalCost)
         }
         logEntry.Print(INFO, correlationId, []string {
-          fmt.Sprintf("n = %s, tp = %s, interest = %s, cp = %s, amount = %s, %s", mf.fd1N,
-            mf.fd1TimePeriod, mf.fd1Interest, mf.fd1Compound, mf.fd1Amount, mf.fd1Result[0]),
+          fmt.Sprintf("n = %s, tp = %s, interest = %s, cp = %s, amount = %s, %s", mf.Fd1N,
+            mf.Fd1TimePeriod, mf.Fd1Interest, mf.Fd1Compound, mf.Fd1Amount, mf.Fd1Result[0]),
         })
       }
       newSessionToken, newSession := sessions.UpdateEntryInSessions(sessionToken)
@@ -114,43 +118,43 @@ func (mp WfMortgagePages) MortgagePages(res http.ResponseWriter, req *http.Reque
         Fd1Compound string
         Fd1Amount string
         Fd1Result [3]string
-      } { "Mortgage", m.DTF(), mf.currentButton, newSession.CsrfToken,
-          mf.fd1N, mf.fd1TimePeriod, mf.fd1Interest, mf.fd1Compound, mf.fd1Amount, mf.fd1Result,
+      } { "Mortgage", m.DTF(), mf.CurrentButton, newSession.CsrfToken,
+          mf.Fd1N, mf.Fd1TimePeriod, mf.Fd1Interest, mf.Fd1Compound, mf.Fd1Amount, mf.Fd1Result,
         })
-    } else if strings.EqualFold(mf.currentPage, "rhs-ui2") {
-      mf.currentButton = "lhs-button2"
+    } else if strings.EqualFold(mf.CurrentPage, "rhs-ui2") {
+      mf.CurrentButton = "lhs-button2"
       if req.Method == http.MethodPost {
-        mf.fd2N = req.FormValue("fd2-n")
-        mf.fd2TimePeriod = req.PostFormValue("fd2-tp")
-        mf.fd2Interest = req.PostFormValue("fd2-i")
-        mf.fd2Compound = req.PostFormValue("fd2-compound")
-        mf.fd2Amount = req.PostFormValue("fd2-amount")
+        mf.Fd2N = req.FormValue("fd2-n")
+        mf.Fd2TimePeriod = req.PostFormValue("fd2-tp")
+        mf.Fd2Interest = req.PostFormValue("fd2-i")
+        mf.Fd2Compound = req.PostFormValue("fd2-compound")
+        mf.Fd2Amount = req.PostFormValue("fd2-amount")
         var n float64
         var i float64
         var amount float64
         var err error
-        if n, err = strconv.ParseFloat(mf.fd2N, 64); err != nil {
-          mf.fd2Result = append(mf.fd2Result,
+        if n, err = strconv.ParseFloat(mf.Fd2N, 64); err != nil {
+          mf.Fd2Result = append(mf.Fd2Result,
             Row {
-              PaymentNo: fmt.Sprintf("Error: %s -- %+v", mf.fd2N, err),
+              PaymentNo: fmt.Sprintf("Error: %s -- %+v", mf.Fd2N, err),
             })
-        } else if i, err = strconv.ParseFloat(mf.fd2Interest, 64); err != nil {
-          mf.fd2Result = append(mf.fd2Result,
+        } else if i, err = strconv.ParseFloat(mf.Fd2Interest, 64); err != nil {
+          mf.Fd2Result = append(mf.Fd2Result,
             Row {
-              PaymentNo: fmt.Sprintf("Error: %s -- %+v", mf.fd2Interest, err),
+              PaymentNo: fmt.Sprintf("Error: %s -- %+v", mf.Fd2Interest, err),
             })
-        } else if amount, err = strconv.ParseFloat(mf.fd2Amount, 64); err != nil {
-          mf.fd2Result = append(mf.fd2Result,
+        } else if amount, err = strconv.ParseFloat(mf.Fd2Amount, 64); err != nil {
+          mf.Fd2Result = append(mf.Fd2Result,
             Row {
-              PaymentNo: fmt.Sprintf("Error: %s -- %+v", mf.fd2Amount, err),
+              PaymentNo: fmt.Sprintf("Error: %s -- %+v", mf.Fd2Amount, err),
             })
         } else {
           var m finances.Mortgage
-          var at = m.AmortizationTable(amount, i / 100.0, mf.fd1Compound[0], n,
-            mf.fd1TimePeriod[0])
+          var at = m.AmortizationTable(amount, i / 100.0, mf.Fd1Compound[0], n,
+            mf.Fd1TimePeriod[0])
           var numberOfRows = len(at.Rows)
-          mf.fd2Result = make([]Row, 0, numberOfRows + 1)
-          mf.fd2Result = append(mf.fd2Result,
+          mf.Fd2Result = make([]Row, 0, numberOfRows + 1)
+          mf.Fd2Result = append(mf.Fd2Result,
             Row {
               PaymentNo: "--",
               Payment: "--",
@@ -159,7 +163,7 @@ func (mp WfMortgagePages) MortgagePages(res http.ResponseWriter, req *http.Reque
               Balance: fmt.Sprintf("%.2f", amount),
             })
           for idx := 0; idx < numberOfRows; idx++ {
-            mf.fd2Result = append(mf.fd2Result,
+            mf.Fd2Result = append(mf.Fd2Result,
               Row {
                 PaymentNo: fmt.Sprintf("%v", idx + 1),
                 Payment: fmt.Sprintf("%.2f", at.Rows[idx].Payment),
@@ -168,13 +172,13 @@ func (mp WfMortgagePages) MortgagePages(res http.ResponseWriter, req *http.Reque
                 Balance: fmt.Sprintf("%.2f", at.Rows[idx].Balance),
               })
           }
-          mf.fd2TotalCost = fmt.Sprintf("Total Cost: $%.2f", at.TotalCost)
-          mf.fd2TotalInterest = fmt.Sprintf("Total Interest: $%.2f", at.TotalInterest)
+          mf.Fd2TotalCost = fmt.Sprintf("Total Cost: $%.2f", at.TotalCost)
+          mf.Fd2TotalInterest = fmt.Sprintf("Total Interest: $%.2f", at.TotalInterest)
         }
         logEntry.Print(INFO, correlationId, []string {
           fmt.Sprintf("n = %s, tp = %s, interest = %s, cp = %s, amount = %s, total cost = %s, total interest = %s",
-            mf.fd2N, mf.fd2TimePeriod, mf.fd2Interest, mf.fd2Compound, mf.fd2Amount,
-            mf.fd2TotalCost, mf.fd2TotalInterest),
+            mf.Fd2N, mf.Fd2TimePeriod, mf.Fd2Interest, mf.Fd2Compound, mf.Fd2Amount,
+            mf.Fd2TotalCost, mf.Fd2TotalInterest),
         })
       }
       newSessionToken, newSession := sessions.UpdateEntryInSessions(sessionToken)
@@ -197,38 +201,38 @@ func (mp WfMortgagePages) MortgagePages(res http.ResponseWriter, req *http.Reque
         Fd2TotalCost string
         Fd2TotalInterest string
         Fd2Result []Row
-      } { "Mortgage", m.DTF(), mf.currentButton, newSession.CsrfToken, mf.fd2N, mf.fd2TimePeriod,
-          mf.fd2Interest, mf.fd2Compound, mf.fd2Amount, mf.fd2TotalCost, mf.fd2TotalInterest,
-          mf.fd2Result,
+      } { "Mortgage", m.DTF(), mf.CurrentButton, newSession.CsrfToken, mf.Fd2N, mf.Fd2TimePeriod,
+          mf.Fd2Interest, mf.Fd2Compound, mf.Fd2Amount, mf.Fd2TotalCost, mf.Fd2TotalInterest,
+          mf.Fd2Result,
         })
-    } else if strings.EqualFold(mf.currentPage, "rhs-ui3") {
-      mf.currentButton = "lhs-button3"
+    } else if strings.EqualFold(mf.CurrentPage, "rhs-ui3") {
+      mf.CurrentButton = "lhs-button3"
       if req.Method == http.MethodPost {
-        mf.fd3Mrate = req.PostFormValue("fd3-mrate")
-        mf.fd3Mbalance = req.PostFormValue("fd3-mbalance")
-        mf.fd3Hrate = req.PostFormValue("fd3-hrate")
-        mf.fd3Hbalance = req.PostFormValue("fd3-hbalance")
+        mf.Fd3Mrate = req.PostFormValue("fd3-mrate")
+        mf.Fd3Mbalance = req.PostFormValue("fd3-mbalance")
+        mf.Fd3Hrate = req.PostFormValue("fd3-hrate")
+        mf.Fd3Hbalance = req.PostFormValue("fd3-hbalance")
         var mRate float64
         var mBalance float64
         var hRate float64
         var hBalance float64
         var err error
-        if mRate, err = strconv.ParseFloat(mf.fd3Mrate, 64); err != nil {
-          mf.fd3Result[2] = fmt.Sprintf("Error: %s -- %+v", mf.fd3Mrate, err)
-        } else if mBalance, err = strconv.ParseFloat(mf.fd3Mbalance, 64); err != nil {
-          mf.fd3Result[2] = fmt.Sprintf("Error: %s -- %+v", mf.fd3Mbalance, err)
-        } else if hRate, err = strconv.ParseFloat(mf.fd3Hrate, 64); err != nil {
-          mf.fd3Result[2] = fmt.Sprintf("Error: %s -- %+v", mf.fd3Hrate, err)
-        } else if hBalance, err = strconv.ParseFloat(mf.fd3Hbalance, 64); err != nil {
-          mf.fd3Result[2] = fmt.Sprintf("Error: %s -- %+v", mf.fd3Hbalance, err)
+        if mRate, err = strconv.ParseFloat(mf.Fd3Mrate, 64); err != nil {
+          mf.Fd3Result[2] = fmt.Sprintf("Error: %s -- %+v", mf.Fd3Mrate, err)
+        } else if mBalance, err = strconv.ParseFloat(mf.Fd3Mbalance, 64); err != nil {
+          mf.Fd3Result[2] = fmt.Sprintf("Error: %s -- %+v", mf.Fd3Mbalance, err)
+        } else if hRate, err = strconv.ParseFloat(mf.Fd3Hrate, 64); err != nil {
+          mf.Fd3Result[2] = fmt.Sprintf("Error: %s -- %+v", mf.Fd3Hrate, err)
+        } else if hBalance, err = strconv.ParseFloat(mf.Fd3Hbalance, 64); err != nil {
+          mf.Fd3Result[2] = fmt.Sprintf("Error: %s -- %+v", mf.Fd3Hbalance, err)
         } else {
           var m finances.Mortgage
-          mf.fd3Result[2] = fmt.Sprintf("Blended Interest Rate: %.3f%%",
+          mf.Fd3Result[2] = fmt.Sprintf("Blended Interest Rate: %.3f%%",
             m.BlendedInterestRate(mBalance, mRate, hBalance, hRate))
         }
         logEntry.Print(INFO, correlationId, []string {
           fmt.Sprintf("mortgage balance = %s, mortgage rate = %s, HELOC balance = %s, HELOC rate = %s, %s\n",
-            mf.fd3Mbalance, mf.fd3Mrate, mf.fd3Hbalance, mf.fd3Hrate, mf.fd3Result[2]),
+            mf.Fd3Mbalance, mf.Fd3Mrate, mf.Fd3Hbalance, mf.Fd3Hrate, mf.Fd3Result[2]),
         })
       }
       newSessionToken, newSession := sessions.UpdateEntryInSessions(sessionToken)
@@ -248,29 +252,42 @@ func (mp WfMortgagePages) MortgagePages(res http.ResponseWriter, req *http.Reque
         Fd3Hrate string
         Fd3Hbalance string
         Fd3Result [3]string
-      } { "Mortgage", m.DTF(), mf.currentButton, newSession.CsrfToken, mf.fd3Mrate, mf.fd3Mbalance,
-          mf.fd3Hrate, mf.fd3Hbalance, mf.fd3Result,
+      } { "Mortgage", m.DTF(), mf.CurrentButton, newSession.CsrfToken, mf.Fd3Mrate, mf.Fd3Mbalance,
+          mf.Fd3Hrate, mf.Fd3Hbalance, mf.Fd3Result,
         })
     } else {
-      errString := fmt.Sprintf("Unsupported page: %s", mf.currentPage)
+      errString := fmt.Sprintf("Unsupported page: %s", mf.CurrentPage)
       fmt.Printf("%s - %s\n", m.DTF(), errString)
       panic(errString)
     }
     //
     if req.Context().Err() == context.DeadlineExceeded {
       fmt.Println("*** Request timeout ***")
-      if strings.EqualFold(mf.currentPage, "rhs-ui1") {
-        mf.fd1Result[0] = ""
-        mf.fd1Result[1] = ""
-        mf.fd1Result[2] = ""
-      } else if strings.EqualFold(mf.currentPage, "rhs-ui2") {
-        mf.fd2Result = nil
-        mf.fd2TotalCost = ""
-        mf.fd2TotalInterest = ""
-      } else if strings.EqualFold(mf.currentPage, "rhs-ui3") {
-        mf.fd3Result[2] = ""
+      if strings.EqualFold(mf.CurrentPage, "rhs-ui1") {
+        mf.Fd1Result[0] = ""
+        mf.Fd1Result[1] = ""
+        mf.Fd1Result[2] = ""
+      } else if strings.EqualFold(mf.CurrentPage, "rhs-ui2") {
+        mf.Fd2Result = nil
+        mf.Fd2TotalCost = ""
+        mf.Fd2TotalInterest = ""
+      } else if strings.EqualFold(mf.CurrentPage, "rhs-ui3") {
+        mf.Fd3Result[2] = ""
       }
     }
+
+    /////////////
+    
+    if data, err := json.Marshal(mf); err != nil {
+      fmt.Printf("%s - %s\n", m.DTF(), err)
+    } else {
+      filePath := fmt.Sprintf("%s/%s/mortgage.bin", mainDir, userName)
+      if _, err = misc.WriteAllExclusiveLock(filePath, data, os.O_WRONLY, 0o220); err != nil {
+        fmt.Printf("%s - %s\n", m.DTF(), err)
+      }
+    }
+/////////////////
+
   } else {
     errString := fmt.Sprintf("Unsupported method: %s", req.Method)
     fmt.Printf("%s - %s\n", m.DTF(), errString)
