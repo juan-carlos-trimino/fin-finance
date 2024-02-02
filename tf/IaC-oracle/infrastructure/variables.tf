3932b17051fea2452a13d6498567bae588963470
Juan Carlos Trimino <juancarlos@trimino.com>
2024-02-01 19:27:53 -0600

    initial import.

---
+++ tf/IaC-oracle/infrastructure/variables.tf
@@ -0,0 +1,14 @@
+####################
+# GLOBAL VARIABLES #
+####################
+variable app_name {
+  type = string
+  default = "finances"
+  description = "The name of the application."
+}
+
+variable app_version {
+  type = string
+  default = "1.0.0"
+  description = "The application version."
+}
