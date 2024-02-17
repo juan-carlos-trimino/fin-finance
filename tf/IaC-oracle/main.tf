6cc707af7681ae5b4db1be1b37f024ef383f83c7
Juan Carlos Trimino <juancarlos@trimino.com>
2024-02-16 21:06:16 -0600

    add comments.

--- tf/IaC-oracle/main.tf
+++ tf/IaC-oracle/main.tf
@@ -36,6 +36,12 @@
 # $ terraform destroy -var="app_version=1.0.0" -auto-approve
 # $ terraform version
 
+# $ terraform init
+# $ terraform -chdir=../tf init
+# where -chdir=../tf allows you to declare where the root of your terraform project is located.
+# $ terraform apply -var="app_version=1.0.0" -auto-approve
+# $ terraform destroy -var="app_version=1.0.0" -auto-approve
+# $ terraform version
 
 # To troubleshoot the OCI Terraform Provider:
 # https://docs.oracle.com/en-us/iaas/Content/API/SDKDocs/terraformtroubleshooting.htm
