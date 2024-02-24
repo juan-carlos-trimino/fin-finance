cf41557a43c8d1835e3c43900c16635ab8bbe55b
Juan Carlos Trimino <juancarlos@trimino.com>
2024-02-24 00:34:53 -0600

    change dir and readiness_probe.

--- fin-IaC-app/main.tf
+++ fin-IaC-app/main.tf
@@ -1,3 +1,10 @@
+
+# Execute the following command to clean up all the pending resources and remove all the K8s
+# objects: (Caution: The command in this step removes all the resources.)
+# $ kubectl delete all --all -n <namespace>
+# Execute the following command to delete the namespace:
+# $ kubectl delete namespace <namespace>
+
 locals {
   namespace = kubernetes_namespace.ns.metadata[0].name
   cr_login_server = "docker.io"
@@ -18,7 +25,7 @@ module "fin-finances" {
 #  count = var.k8s_manifest_crd ? 0 : 1
   # Specify the location of the module, which contains the file main.tf.
   source = "./modules/deployment"
-dir_name = "../"
+  dir_path = ".."
   app_name = var.app_name
   app_version = var.app_version
   namespace = local.namespace
@@ -32,23 +39,22 @@ dir_name = "../"
   env = {
     SVC_NAME: local.svc_finances
     APP_NAME_VER: "${var.app_name} ${var.app_version}"
-    K8S: true
     PORT: "80"
     MAX_RETRIES: 20
     SERVER: "http://${local.svc_dns_finances}"
   }
-  readiness_probe = [{
-    http_get = [{
-      path = "/readiness"
-      port = 0
-      scheme = "HTTP"
-    }]
-    initial_delay_seconds = 30
-    period_seconds = 20
-    timeout_seconds = 2
-    failure_threshold = 4
-    success_threshold = 1
-  }]
+  # readiness_probe = [{
+  #   http_get = [{
+  #     path = "/readiness"
+  #     port = 0
+  #     scheme = "HTTP"
+  #   }]
+  #   initial_delay_seconds = 30
+  #   period_seconds = 20
+  #   timeout_seconds = 2
+  #   failure_threshold = 4
+  #   success_threshold = 1
+  # }]
   service_name = local.svc_finances
   service_type = "LoadBalancer"
 }
