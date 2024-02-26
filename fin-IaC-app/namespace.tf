c2ecdd48ddfed86a787083e9e67f0f20ce9622f4
Juan Carlos Trimino <juancarlos@trimino.com>
2024-02-25 19:07:16 -0600

    add comments.

--- fin-IaC-app/namespace.tf
+++ fin-IaC-app/namespace.tf
@@ -1,5 +1,10 @@
 # $ kubectl get ns
 # $ kubectl describe ns
+# Execute the following command to clean up all the pending resources and remove all the K8s
+# objects: (Caution: The command in this step removes all the resources.)
+# $ kubectl delete all --all -n <namespace>
+# Execute the following command to delete the namespace:
+# $ kubectl delete namespace <namespace>
 resource "kubernetes_namespace" "ns" {
   metadata {
     name = var.app_name
