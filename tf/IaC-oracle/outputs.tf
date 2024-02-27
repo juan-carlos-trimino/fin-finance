cc0c358159f33da09977f89ed22a64f3c69dfd37
Juan Carlos Trimino <juancarlos@trimino.com>
2024-02-26 19:52:23 -0600

    add output for the node port nlb public ip.

--- tf/IaC-oracle/outputs.tf
+++ tf/IaC-oracle/outputs.tf
@@ -1,14 +1,21 @@
-##############################
-# Outputs for the vcn module #
-##############################
+#############################
+# Output for the vcn module #
+#############################
 output "vcn_id" {
   description = "OCID of the VCN that is created"
   value = module.vcn.vcn_id
 }
 
-###########################
-# Outputs for compartment #
-###########################
-output "compartment-id" {
+###########################################
+# Output for compartment (compartment.tf) #
+###########################################
+output "compartment_id" {
   value = oci_identity_compartment.fin-compartment.id
+}
+
+#######################################################################
+# Output for Node Port Network Load Balancer (NLB) (node-port-nlb.tf) #
+#######################################################################
+output "node_port_nlb_public_ip" {
+  value = [for ip in oci_network_load_balancer_network_load_balancer.node-port-nlb.ip_addresses : ip if ip.is_public == true]
 }
