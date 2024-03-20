ef7035772676bab0bde1d03ce7bcda053e322725
Juan Carlos Trimino <juancarlos@trimino.com>
2024-03-19 21:13:07 -0500

    remove variables.

--- IaC-app/variables.tf
+++ IaC-app/variables.tf
@@ -31,7 +31,7 @@ variable k8s_manifest_crd {
   default = "true"
 }
 
-variable nlb_node_port {
+variable nlb_node_port {  # Network load balancer.
   type = number
   default = 31600
 }
@@ -46,24 +46,6 @@ variable nlb_node_port {
 # IMPORTANT: Because this file contains confidential/sensitive information, do not push the file  #
 #            to a version control system. This file is meant to be on your local system only.     #
 ###################################################################################################
-variable compartment_id {
-  description = "Organize and control access to your resources."
-  type = string
-  sensitive = true
-}
-
-variable public_subnet_id {
-  type = string
-  sensitive = true
-}
-
-# Navigation menu->Developer Services->Containers & Artifacts [Under]->Kubernetes Clusters (OKE)->
-# Compartment that contains the cluster->Clusters page->Resources [Under]->Node pools
-variable node_pool_id {
-  type = string
-  sensitive = true
-}
-
 variable region {
   description = "An OCI region."
   type = string
