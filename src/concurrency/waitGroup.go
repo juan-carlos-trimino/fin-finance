6d643d593be5db2edca4ed67b684dfdde3a5df30
Juan Carlos Trimino <juancarlos@trimino.com>
2024-04-11 11:40:03 -0500

    add comments.

--- src/concurrency/waitGroup.go
+++ src/concurrency/waitGroup.go
@@ -4,7 +4,13 @@ import (
   "sync"
 )
 
+/***
+This implementation does not have the limitations described in the WaitGrpL; i.e., the count of the
+waitgroup can be changed after creation and more than one goroutine can be suspended and unblocked
+on the Wait() function.
+***/
 type WaitGrp struct {
+  //The waitgroup count; Go initializes it to 0 by default.
   count int
   cond *sync.Cond
 }
@@ -12,27 +18,32 @@ type WaitGrp struct {
 func NewWaitGrp() *WaitGrp {
   return &WaitGrp{
     count: 0,
+    //Initialize the condition variable with a mutex.
     cond: sync.NewCond(&sync.Mutex{}),
   }
 }
 
 func (wg *WaitGrp) Add(delta int) {
-  wg.cond.L.Lock()
+  wg.cond.L.Lock()  //Protect count.
   wg.count += delta
   wg.cond.L.Unlock()
 }
 
 func (wg *WaitGrp) Wait() {
+  //Protect the read of count with a mutex lock on the condition variable.
   wg.cond.L.Lock()
   for wg.count > 0 {
+    //Wait and release the mutex atomically while count is greater than 0.
     wg.cond.Wait()
   }
   wg.cond.L.Unlock()
 }
 
 func (wg *WaitGrp) Done() {
-  wg.cond.L.Lock()
+  wg.cond.L.Lock()  //Protect count.
   wg.count--
+  //If it's the last goroutine to be done in the waitgroup, it broadcasts on the condition
+  //variable.
   if wg.count == 0 {
     wg.cond.Broadcast()
   }
