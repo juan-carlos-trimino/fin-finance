751262f774722f6f15b83cc5a7d999d86cd56a0d
Juan Carlos Trimino <juancarlos@trimino.com>
2024-09-02 11:04:30 -0500

    change k8s version.

--- IaC-K8s/oracle/main.tf
+++ IaC-K8s/oracle/main.tf
@@ -226,7 +226,8 @@ module "cluster" {
   type = "BASIC_CLUSTER"
   compartment_id = oci_identity_compartment.fin-compartment.id
   vcn_id = module.vcn.vcn_id
-  k8s_version = "v1.29.1"
+  # k8s_version = "v1.29.1"
+  k8s_version = "v1.30.1"
   subnet_ids = [
     module.public-subnet.subnet-id
   ]
