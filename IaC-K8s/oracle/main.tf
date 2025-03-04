6a29d586d94ebac1879717e66e43c4b0c451c5f1
Juan Carlos Trimiño <juancarlos@trimino.com>
2025-03-03 21:44:30 -0600

    add the repo vs-code-settings.

--- IaC-K8s/oracle/main.tf
+++ IaC-K8s/oracle/main.tf
@@ -1,24 +1,4 @@
 ###################################################################################################
-# $ terraform init
-# $ terraform -chdir=../tf init
-#   where -chdir=../tf allows you to declare where the root of your terraform project is located.
-#
-# $ terraform plan
-# $ terraform plan -var-file="../tf_secrets.auto.tfvars"
-#
-# $ terraform apply
-# $ terraform apply -auto-approve
-# $ terraform apply -var-file="../tf_secrets.auto.tfvars"
-# $ terraform apply -var="app_version=1.0.0" -auto-approve
-#
-###################################################################################################
-# IMPORTANT: Resources you provision accrue costs while they are running. It's a good idea, as you#
-#            learn, to always run "terraform destroy" on your project.                            #
-###################################################################################################
-# $ terraform destroy
-# $ terraform destroy -auto-approve
-# $ terraform destroy -var-file="../tf_secrets.auto.tfvars"
-# $ terraform destroy -var="app_version=1.0.0" -auto-approve
 #
 # To troubleshoot the OCI Terraform Provider:
 # https://docs.oracle.com/en-us/iaas/Content/API/SDKDocs/terraformtroubleshooting.htm
