f4e55be2bee8fa32c986489d131bd1d1a3abae22
Juan Carlos Trimiño <juancarlos@trimino.com>
2025-05-09 13:32:12 -0500

    initial import.

---
+++ IaC-app/deploy.sh
@@ -0,0 +1,38 @@
+
+# Treats unset or undefined variables as an error when substituting (during parameter expansion).
+# Does not apply to special parameters such as wildcard * or @.
+set -u
+# Checks that expected input environment variables are provided.
+: "$APP_VERSION"
+: "$K8S_MANIFEST_CRD"
+echo "*********************"
+echo "Environment variables"
+echo "*********************"
+echo "APP_VERSION = $(printenv APP_VERSION)"
+echo "K8S_MANIFEST_CRD = $(printenv K8S_MANIFEST_CRD)"
+# cd ./fin-finance/IaC-app/
+echo "*****************"
+echo "Current directory"
+echo "*****************"
+echo $(pwd)
+echo "*************************************************************"
+echo "Creating the directory structure for the Terraform state file"
+echo "*************************************************************"
+# If the directory already exists, mkdir will not create it again and will not produce an error.
+# To preserve linebreaks, quote the command or variable.
+echo "$(mkdir --verbose --parents ../../../tf-states/IaC-app/)"
+echo "$(chmod -v -R 700 ../../../tf-states/)"
+echo "*************************"
+echo "Deploying the application"
+echo "*************************"
+terraform init
+# terraform apply -auto-approve \
+#   -var "app_version=$APP_VERSION" \
+#   -var "k8s_manifest_crd=$K8S_MANIFEST_CRD"
+echo "*********************************"
+echo "Saving the vars file with secrets"
+echo "*********************************"
+echo "$(mkdir --verbose --parents ../../../tf-secret-vars/IaC-app/)"
+echo "$(chmod -v -R 700 ../../../tf-secret-vars/)"
+echo "$(cp -v --update --archive tf_secrets.auto.tfvars ../../../tf-secret-vars/IaC-app/tf_secrets.auto.tfvars)"
+echo "Done"
---
+++ IaC-app/set-ev.sh
@@ -0,0 +1,17 @@
+
+# The "Dot Space Script" Trick for Running Bash Scripts in the "Current Shell"
+# In Bash, a dot (".") is a command, which is synonymous with the "source" command. It executes the
+# content of a specified script in the current shell environment. This differs from running a
+# script directly, which executes in a subshell. Therefore, when using ".", any changes made by the
+# script, such as variables assignments or directory changes, will persist in the current shell. To
+# persist the environment variables, run the script like so
+# $ . ./ev_app.sh
+# or
+# $ source ./ev_app.sh
+# Please note that "source" is a synonym for dot (".") in the Bash shell, but not in POSIX sh, so
+# for maximum compatibility use the period.
+#
+export APP_VERSION=1.0.0
+echo $(printenv APP_VERSION)
+export K8S_MANIFEST_CRD=false
+echo $(printenv K8S_MANIFEST_CRD)
