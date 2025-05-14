1c460fd93be942b90d69f2044c4f2d5cb9c99537
Juan Carlos Trimiño <juancarlos@trimino.com>
2025-05-14 16:12:34 -0500

    add comment for flag use_lockfile.

--- IaC-app/backend.tf
+++ IaC-app/backend.tf
@@ -2,5 +2,7 @@
 terraform {
   backend "local" {
     path = "../../../tf-states/IaC-app/terraform.tfstate"
+    # To prevent S3-native locking (or moving the lock file), set this to false.
+    # use_lockfile = false
   }
 }
