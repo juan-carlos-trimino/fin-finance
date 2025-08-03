5ad346bfd897cf7909654f3be39e4f9dc7337435
Juan Carlos Trimiño <juancarlos@trimino.com>
2025-08-03 18:56:20 -0500

    change variables for postgres.

--- IaC-app/variables.tf
+++ IaC-app/variables.tf
@@ -192,25 +192,46 @@ variable traefik_le_email {
 variable postgres_image_tag {  # https://www.postgresql.org/docs/release/
   description = "PostgreSQL Docker images (https://hub.docker.com/_/postgres)."
   type = string
-  default = "postgres:17.5-alpine3.22"
+  default = "postgres:17.5"
 }
 
+/***
+This optional environment variable can be used to define a different name for the default database
+that is created when the image is first started. If it is not specified, then the value of
+POSTGRES_USER will be used.
+***/
 variable postgres_db {
-  description = "POSTGRES_DB: xxxxxxxxxxxxxxxxxxxxxxxxxSecret for PostgreSQL Authentication."
+  description = "POSTGRES_DB: The default database."
   default = "<required>"
   sensitive = true
   type = string
 }
 
+/***
+This optional environment variable is used in conjunction with POSTGRES_PASSWORD to set a user and
+its password. This variable will create the specified user with superuser power and a database with
+the same name. If it is not specified, then the default user of 'postgres' will be used.
+***/
 variable postgres_user {
-  description = "POSTGRES_USER: xxxxxxxxxxxxxxxxxxxxxxxxxSecret for PostgreSQL Authentication."
+  description = "POSTGRES_USER: It defines the default superuser."
   default = "<required>"
   sensitive = true
   type = string
 }
 
+/***
+The only variable required is POSTGRES_PASSWORD, the rest are optional.
+Note 1: The PostgreSQL image sets up 'trust' authentication locally so you may notice a password is
+not required when connecting from localhost (inside the same container). However, a password will
+be required if connecting from a different host/container.
+Note 2: This variable defines the superuser password in the PostgreSQL instance, as set by the
+initdb script during initial container startup. It has no effect on the PGPASSWORD environment
+variable that may be used by the psql client at runtime, as described at
+https://www.postgresql.org/docs/14/libpq-envars.html⁠. PGPASSWORD, if used, will be specified as a
+separate environment variable.
+***/
 variable postgres_password {
-  description = "POSTGRES_PASSWORD: Secret for PostgreSQL Authentication."
+  description = "POSTGRES_PASSWORD: It sets the superuser password for PostgreSQL."
   default = "<required>"
   sensitive = true
   type = string
@@ -230,9 +251,16 @@ variable replication_password {
   type = string
 }
 
+variable pgdata {
+  description = "PGDATA:xxxxxxxxxxxxxxxxxxxxxxxx"
+  default = "/wsf_data_dir/data/pgdata"
+  type = string
+}
 
-
-
+variable path_postgres_confs {
+  default = "./config/postgres"
+  type = string
+}
 
 ################
 # MySQL Server #
