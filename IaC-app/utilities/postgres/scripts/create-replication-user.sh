f03e133ea85979d771f966eb1aeacbdabb4c4583
Juan Carlos Trimiño <juancarlos@trimino.com>
2025-08-08 20:48:28 -0500

    initial commit.

---
+++ IaC-app/utilities/postgres/scripts/create-replication-user.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# The '-e' option tells the shell to exit immediately if any invoked command exits with a non-zero
+# status, which is the convention for indicating errors in Unix-like operating systems. It's a
+# handy option for scripting, as it helps to catch errors and bugs early.
+set -e
+
+# The key difference with <<- (compared to just <<) is that it strips all leading tab characters
+# from the input lines within the here document (heredoc). This is useful for indentation, allowing
+# you to format your code nicely without affecting the actual input data.
+# https://www.postgresql.org/docs/current/app-psql.html
+psql -v ON_ERROR_STOP=1 --username "$REPLICATION_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE ROLE replication WITH REPLICATION PASSWORD '$REPLICATION_PASSWORD' LOGIN
+EOSQL
