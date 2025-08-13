416db45cdecdf949d63e1b8666f05e3899e22794
Juan Carlos Trimiño <juancarlos@trimino.com>
2025-08-12 23:14:04 -0500

    use the REPLICATION_USER environment variable.

--- IaC-app/utilities/postgres/scripts/create-replication-user.sh
+++ IaC-app/utilities/postgres/scripts/create-replication-user.sh
@@ -7,7 +7,8 @@ set -e
 # The key difference with <<- (compared to just <<) is that it strips all leading tab characters
 # from the input lines within the here document (heredoc). This is useful for indentation, allowing
 # you to format your code nicely without affecting the actual input data.
+# https://www.postgresql.org/docs/current/warm-standby.html#STREAMING-REPLICATION
 # https://www.postgresql.org/docs/current/app-psql.html
 psql -v ON_ERROR_STOP=1 --username "$REPLICATION_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-  CREATE ROLE replication WITH REPLICATION PASSWORD '$REPLICATION_PASSWORD' LOGIN
+  CREATE ROLE '$REPLICATION_USER' WITH REPLICATION PASSWORD '$REPLICATION_PASSWORD' LOGIN;
 EOSQL
