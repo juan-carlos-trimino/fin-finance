2b6a06689b57ef1c62546bb5d8cea0a02ecb2e92
Juan Carlos Trimiño <juancarlos@trimino.com>
2025-09-08 09:09:11 -0500

    add postgres password.

--- IaC-app/utilities/postgres/scripts/postgres-backup.sh
+++ IaC-app/utilities/postgres/scripts/postgres-backup.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
-BACKUP_DIR="/wsf_data_dir/backups"
+BACKUP_DIR="/wsf_data_dir/postgres/backups"
 printf "Backup directory: $BACKUP_DIR\n"
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 
+mkdir -p /wsf_dir/postgres/backups
 
-export PGPASSWORD="PG_PASSWORD"
-
-printf "pgpaasword=$PGPASSWORD\n"
-printf "pgdb=$POSTGRES_DB\n"
-printf "pguser=$POSTGRES_USER\n"
-
-
-# pg_dump -U "$DB_USER" "$DB_NAME" > "$BACKUP_DIR/$DB_NAME-$TIMESTAMP.sql"
-
-# pg_dump -h remote_host -p 5432 -U db_user db_name
-
-# pg_basebackup -v -h $PGHOST -p 5432 -D $PGDATA -U replication -R -Xs -Fp
+export PGPASSWORD=$POSTGRES_PASSWORD
+# Backup.
+pg_dump -h "$PGHOST" -U "$POSTGRES_USER" "$POSTGRES_DB" > "$BACKUP_DIR/$TIMESTAMP_$POSTGRES_DB.sql"
+# sleep 120s
+printf "Backup created at $BACKUP_DIR"
 
 # Optional: Compress the backup
 # gzip "$BACKUP_DIR/$DB_NAME-$TIMESTAMP.sql"
