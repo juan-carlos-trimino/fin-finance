084d29d010d129245a86220450566abf2ff14172
Juan Carlos Trimiño <juancarlos@trimino.com>
2025-09-09 19:07:39 -0500

    clean the file.

--- IaC-app/utilities/postgres/scripts/postgres-backup.sh
+++ IaC-app/utilities/postgres/scripts/postgres-backup.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-BACKUP_DIR="/wsf_data_dir/postgres/backups"
+BACKUP_DIR="/wsf_data_dir/backups"
 printf "Backup directory: $BACKUP_DIR\n"
-TIMESTAMP=$(date +%Y%m%d_%H%M%S)
-
-mkdir -p /wsf_dir/postgres/backups
-
+printf "Creating the backup directory...\n"
+mkdir -p /wsf_data_dir/backups
 export PGPASSWORD=$POSTGRES_PASSWORD
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 # Backup.
-pg_dump -h "$PGHOST" -U "$POSTGRES_USER" "$POSTGRES_DB" > "$BACKUP_DIR/$TIMESTAMP_$POSTGRES_DB.sql"
-# sleep 120s
+pg_dump -v -h "$PGHOST" -p 5432 -U "$POSTGRES_USER" "$POSTGRES_DB" > "$BACKUP_DIR/${TIMESTAMP}_$POSTGRES_DB.sql"
+sleep 300s
 printf "Backup created at $BACKUP_DIR"
-
 # Optional: Compress the backup
 # gzip "$BACKUP_DIR/$DB_NAME-$TIMESTAMP.sql"
