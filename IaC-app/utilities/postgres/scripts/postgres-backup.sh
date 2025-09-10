bada50720a2bc7eb3f4ee2b6d09b12756ad1ae36
Juan Carlos Trimiño <juancarlos@trimino.com>
2025-09-09 20:57:36 -0500

    remove the sleep function from the script.

--- IaC-app/utilities/postgres/scripts/postgres-backup.sh
+++ IaC-app/utilities/postgres/scripts/postgres-backup.sh
@@ -7,7 +7,7 @@ export PGPASSWORD=$POSTGRES_PASSWORD
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 # Backup.
 pg_dump -v -h "$PGHOST" -p 5432 -U "$POSTGRES_USER" "$POSTGRES_DB" > "$BACKUP_DIR/${TIMESTAMP}_$POSTGRES_DB.sql"
-sleep 300s
-printf "Backup created at $BACKUP_DIR"
-# Optional: Compress the backup
+# sleep 300s
+printf "Backup created at $BACKUP_DIR\n"
+# Optional: Compress the backup.
 # gzip "$BACKUP_DIR/$DB_NAME-$TIMESTAMP.sql"
