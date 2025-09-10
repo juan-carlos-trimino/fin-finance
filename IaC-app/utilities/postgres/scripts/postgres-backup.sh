cc7806d4bb685912415c32e4116767f91f9e6afc
Juan Carlos Trimiño <juancarlos@trimino.com>
2025-09-09 21:42:53 -0500

    add new-line to printf statements.

--- IaC-app/utilities/postgres/scripts/postgres-backup.sh
+++ IaC-app/utilities/postgres/scripts/postgres-backup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 BACKUP_DIR="/wsf_data_dir/backups"
 printf "Backup directory: $BACKUP_DIR\n"
-printf "Creating the backup directory...\n"
+printf "Creating the backup directory...\n\n"
 mkdir -p /wsf_data_dir/backups
 export PGPASSWORD=$POSTGRES_PASSWORD
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 # Backup.
 pg_dump -v -h "$PGHOST" -p 5432 -U "$POSTGRES_USER" "$POSTGRES_DB" > "$BACKUP_DIR/${TIMESTAMP}_$POSTGRES_DB.sql"
 # sleep 300s
-printf "Backup created at $BACKUP_DIR\n"
+printf "\nBackup created at $BACKUP_DIR\n"
 # Optional: Compress the backup.
 # gzip "$BACKUP_DIR/$DB_NAME-$TIMESTAMP.sql"
