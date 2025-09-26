5e74570e88d723a1d7d3f7e1677ab83cf6db1652
Juan Carlos Trimiño <juancarlos@trimino.com>
2025-09-25 21:12:45 -0500

    change variable name.

--- src/config/config.go
+++ src/config/config.go
@@ -15,105 +15,105 @@ func GetServer() string {
   return ev
 }
 
-func GetHttp() bool {
+func GetHttp(correlationId string) bool {
   ev, exists := os.LookupEnv("HTTP")
   if exists {
     v, err := strconv.ParseBool(ev)
     if err == nil {
       return v
     } else {
-      logger.LogInfo(fmt.Sprintf("%s - (Default: true).", err), "-1")
+      logger.LogInfo(fmt.Sprintf("%s - (Default: true).", err), correlationId)
     }
   }
   return true
 }
 
-func GetHttpPort() int {
+func GetHttpPort(correlationId string) int {
   ev, exists := os.LookupEnv("HTTP_PORT")
   if exists {
     v, err := strconv.Atoi(ev)
     if err == nil {
       return v
     } else {
-      logger.LogInfo(fmt.Sprintf("%s - (Default: 8080).", err), "-1")
+      logger.LogInfo(fmt.Sprintf("%s - (Default: 8080).", err), correlationId)
     }
   }
   return 8080
 }
 
-func GetHttps() bool {
+func GetHttps(correlationId string) bool {
   ev, exists := os.LookupEnv("HTTPS")
   if exists {
     v, err := strconv.ParseBool(ev)
     if err == nil {
       return v
     } else {
-      logger.LogInfo(fmt.Sprintf("%s - (Default: false).", err), "-1")
+      logger.LogInfo(fmt.Sprintf("%s - (Default: false).", err), correlationId)
     }
   }
   return false
 }
 
-func GetHttpsPort() int {
+func GetHttpsPort(correlationId string) int {
   ev, exists := os.LookupEnv("HTTPS_PORT")
   if exists {
     v, err := strconv.Atoi(ev)
     if err == nil {
       return v
     } else {
-      logger.LogInfo(fmt.Sprintf("%s - (Default: 8443).", err), "-1")
+      logger.LogInfo(fmt.Sprintf("%s - (Default: 8443).", err), correlationId)
     }
   }
   return 8443
 }
 
-func GetK8s() bool {
+func GetK8s(correlationId string) bool {
   ev, exists := os.LookupEnv("K8S")
   if exists {
     v, err := strconv.ParseBool(ev)
     if err == nil {
       return v
     } else {
-      logger.LogInfo(fmt.Sprintf("%s - (Default: false).", err), "-1")
+      logger.LogInfo(fmt.Sprintf("%s - (Default: false).", err), correlationId)
     }
   }
   return false
 }
 
-func GetShutDownTimeout() int {
+func GetShutDownTimeout(correlationId string) int {
   ev, exists := os.LookupEnv("SHUTDOWN_TIMEOUT")
   if exists {
     v, err := strconv.Atoi(ev)
     if err == nil {
       return v
     } else {
-      logger.LogInfo(fmt.Sprintf("%s - (Default: 15 seconds).", err), "-1")
+      logger.LogInfo(fmt.Sprintf("%s - (Default: 15 seconds).", err), correlationId)
     }
   }
   return 15  //Seconds.
 }
 
-func GetPprof() bool {
+func GetPprof(correlationId string) bool {
   ev, exists := os.LookupEnv("PPROF")
   if exists {
     v, err := strconv.ParseBool(ev)
     if err == nil {
       return v
     } else {
-      logger.LogInfo(fmt.Sprintf("%s - (Default: false).", err), "-1")
+      logger.LogInfo(fmt.Sprintf("%s - (Default: false).", err), correlationId)
     }
   }
   return false
 }
 
-func GetLetsEncryptCert() bool {
+func GetLetsEncryptCert(correlationId string) bool {
   ev, exists := os.LookupEnv("LE_CERT")
   if exists {
     v, err := strconv.ParseBool(ev)
     if err == nil {
       return v
     } else {
-      logger.LogInfo(fmt.Sprintf("%s - (Default: false).", err), "-1")
+      logger.LogInfo(fmt.Sprintf("%s - (Default: false).", err), correlationId)
     }
   }
   return false
@@ -125,4 +125,17 @@ func GetUser() string {
     return ""  //Default value.
   }
   return ev
+}
+
+func GetPreventProbesOutput(correlationId string) bool {
+  ev, exists := os.LookupEnv("PREVENT_PROBES_OUTPUT")
+  if exists {
+    v, err := strconv.ParseBool(ev)
+    if err == nil {
+      return v
+    } else {
+      logger.LogInfo(fmt.Sprintf("%s - (Default: false).", err), correlationId)
+    }
+  }
+  return false
 }
