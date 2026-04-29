f861cc4c6450e0ff01004c8c5c12c325c09b954c
Juan Carlos Trimiño <juancarlos@trimino.com>
2026-04-29 12:59:43 -0500

    fix radio button.

--- src/webfinances/public/js/bondsYTM.js
+++
@@ -1,9 +0,0 @@
-
-function onchangeRadio(radio) {
-  if (radio === 'fd4-curinterest') {
-    document.getElementById("fd4-curinterest").checked = true;
-  } else {
-    document.getElementById("fd4-bondprice").checked = true;
-  }
-  return;
-}
