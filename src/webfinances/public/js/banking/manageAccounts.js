584073cb2e3a6c9aadf12b3f5cf73ca41bcf156e
Juan Carlos Trimiño <juancarlos@trimino.com>
2026-07-26 22:43:31 -0500

    change code since deleteaccount does not have any fields.

--- src/webfinances/public/js/banking/manageAccounts.js
+++ src/webfinances/public/js/banking/manageAccounts.js
@@ -1,36 +1,28 @@
 
 document.addEventListener('DOMContentLoaded', (event) => {
   let params = getParams();
-  // if (params.hasOwnProperty('radio')) {
-  //   disableElements(params.cb);
-  //   setFocus(params.cb);
-  //   onchangeRadio(params.radio);
-  // } else {
-    disableElements(params.cb);
-    setFocus(params.cb);
-  // }
+  disableElements(params.cb);
+  setFocus(params.cb);
 });
 
 function setFocus(eid) {
   // var audio = new Audio('https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3');
-  let tb;
+  let tb = null;
   if (eid === "lhs-button1") {
     // audio.play();
     tb = document.getElementById("fd1-bankname");
-  } else if (eid === "lhs-button2") {
-    tb = document.getElementById("fd2-facevalue");
   }
-  tb.focus();
-  /***
-  input type="number" doesn't support setSelectionRange.
-  You can use type="text" and inputmode="numeric". This will show a numeric keyboard for
-  mobile users and supports setSelectionRange.
-  ***/
-  // tb.type = "text";
-  tb.setSelectionRange(0, tb.value.length);
-  // tb.type = "number";
-  // console.log(`Position start: ${tb.selectionStart}`);
-  // console.log(`Position end: ${tb.selectionEnd}`);
+  //
+  if (tb === null) {
+    tb.focus();
+    /***
+    input type="number" doesn't support setSelectionRange.
+    You can use type="text" and inputmode="numeric". This will show a numeric keyboard for
+    mobile users and supports setSelectionRange.
+    ***/
+    // tb.type = "text";
+    tb.setSelectionRange(0, tb.value.length);
+  }
 }
 
 function disableElements(eid) {
