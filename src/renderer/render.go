f47504381dd86aace9721afa26e59f7bac1efeb3
Juan Carlos Trimiño <juancarlos@trimino.com>
2026-08-12 21:35:22 -0500

    add template functions; render to a temp buffer to catch possible errors.

--- src/renderer/render.go
+++ src/renderer/render.go
@@ -1,6 +1,7 @@
 package renderer
 
 import (
+  "bytes"
   "embed"
   /***
   Package template (html/template) implements data-driven templates for generating HTML output safe against code injection. It
@@ -16,20 +17,38 @@ type PageData struct {
   Data any
 }
 
+//Global template functions available to all templates processed by Render.
+var funcMap = template.FuncMap{
+  "alphabetRange": func(val string) string {
+    switch val {
+    case "1": return "A - G"
+    case "2": return "H - N"
+    case "3": return "O - T"
+    case "4": return "U - Z"
+    default: return "A - G"
+    }
+  },
+  // Add your new functions right here:
+}
+
 func InitTemplates(templateFS embed.FS) {
   GlobalTemplateFS = templateFS
 }
 
 func Render(res http.ResponseWriter, layoutName string, templatePaths []string, data PageData) {
-  res.Header().Set("Content-Type", "text/html; charset=utf-8")
-  //Compile the precise array of files requested by the handler.
-  tmpl, err := template.ParseFS(GlobalTemplateFS, templatePaths...)
+  //Create an empty template root, attach functions, then parse the FS files
+  tmpl, err := template.New(layoutName).Funcs(funcMap).ParseFS(GlobalTemplateFS, templatePaths...)
   if err != nil {
     http.Error(res, "Template parsing failure: " + err.Error(), http.StatusInternalServerError)
-  } else {
-    err = tmpl.ExecuteTemplate(res, layoutName, data)
-    if err != nil {
-      http.Error(res, "Template rendering error: " + err.Error(), http.StatusInternalServerError)
-    }
+    return
   }
+  //Render to a temporary buffer first to catch runtime template errors safely.
+  var buf bytes.Buffer
+  err = tmpl.ExecuteTemplate(&buf, layoutName, data)
+  if err != nil {
+    http.Error(res, "Template rendering error: " + err.Error(), http.StatusInternalServerError)
+    return
+  }
+  res.Header().Set("Content-Type", "text/html; charset=utf-8")
+  buf.WriteTo(res)
 }
