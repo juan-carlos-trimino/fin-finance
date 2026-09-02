6d5ba54d949422a323e4dfb868446bc6875e088e
Juan Carlos Trimiño <juancarlos@trimino.com>
2026-09-01 22:37:26 -0500

    refactor the file.

--- src/webfinances/WfFinAdEppPages.go
+++ src/webfinances/WfFinAdEppPages.go
@@ -1,40 +1,39 @@
 package webfinances
 
 import (
-  "context"
   "encoding/json"
   "finance/finances"
   "finance/renderer"
   "fmt"
-  "github.com/juan-carlos-trimino/gplogger"
-  "github.com/juan-carlos-trimino/go-middlewares"
-  "github.com/juan-carlos-trimino/gposu"
-  "github.com/juan-carlos-trimino/gpsessions"
   "net/http"
   "os"
   "strconv"
   "strings"
   "time"
+  "github.com/juan-carlos-trimino/go-middlewares"
+  osu "github.com/juan-carlos-trimino/go-os"
+  logger "github.com/juan-carlos-trimino/gplogger"
+  sessions "github.com/juan-carlos-trimino/gpsessions"
 )
 
 type adEppFields struct {
-  MenuPage string `json:"menuPage"`
-  CurrentPage string `json:"currentPage"`
+  MenuPage      string `json:"menuPage"`
+  CurrentPage   string `json:"currentPage"`
   CurrentButton string `json:"currentButton"`
   //
-  Fd1N string `json:"fd1N"`
+  Fd1N          string `json:"fd1N"`
   Fd1TimePeriod string `json:"fd1TimePeriod"`
-  Fd1Interest string `json:"fd1Interest"`
-  Fd1Compound string `json:"fd1Compound"`
-  Fd1FV string `json:"fd1FV"`
-  Fd1Result string `json:"fd1Result"`
+  Fd1Interest   string `json:"fd1Interest"`
+  Fd1Compound   string `json:"fd1Compound"`
+  Fd1FV         string `json:"fd1FV"`
+  Fd1Result     string `json:"fd1Result"`
   //
-  Fd2N string `json:"fd2N"`
+  Fd2N          string `json:"fd2N"`
   Fd2TimePeriod string `json:"fd2TimePeriod"`
-  Fd2Interest string `json:"fd2Interest"`
-  Fd2Compound string `json:"fd2Compound"`
-  Fd2PV string `json:"fd2PV"`
-  Fd2Result string `json:"fd2Result"`
+  Fd2Interest   string `json:"fd2Interest"`
+  Fd2Compound   string `json:"fd2Compound"`
+  Fd2PV         string `json:"fd2PV"`
+  Fd2Result     string `json:"fd2Result"`
 }
 
 func newAdEppFields(dir1, dir2, correlationId string) *adEppFields {
@@ -47,14 +46,14 @@ func newAdEppFields(dir1, dir2, correlationId string) *adEppFields {
     Fd1N:          "1.00",
     Fd1TimePeriod: "year",
     Fd1Interest:   "1.00",
-    Fd1Compound:  "annually",
+    Fd1Compound:   "annually",
     Fd1FV:         "1.00",
     Fd1Result:     "",
     //
     Fd2N:          "1.00",
     Fd2TimePeriod: "year",
     Fd2Interest:   "1.00",
-    Fd2Compound:  "annually",
+    Fd2Compound:   "annually",
     Fd2PV:         "1.00",
     Fd2Result:     "",
   }
@@ -67,7 +66,7 @@ func getAdEppFields(userName string) *adEppFields {
   })
 }
 
-type WfAdEppPages struct {}
+type WfAdEppPages struct{}
 
 func (a WfAdEppPages) AdEppPages(res http.ResponseWriter, req *http.Request) {
   ctxKey := middlewares.MwContextKey{}
@@ -96,20 +95,20 @@ func (a WfAdEppPages) AdEppPages(res http.ResponseWriter, req *http.Request) {
   }
   currentFieldsLock.Unlock()
   /***
-  The functions in Request that allow to extract data from the URL and/or the body revolve around the Form, PostForm, and
-  MultipartForm fields; the data are in the form of key-value pairs.
+    The functions in Request that allow to extract data from the URL and/or the body revolve around the Form, PostForm, and
+    MultipartForm fields; the data are in the form of key-value pairs.
 
-  If the form and the URL have the same key name, both of them will be placed in a slice, with the form value always prioritized
-  before the URL value.
+    If the form and the URL have the same key name, both of them will be placed in a slice, with the form value always prioritized
+    before the URL value.
 
-  Since we want the form key-value pairs, we can ignore the URL key-value pairs. The PostForm field provides key-value pairs only
-  for the form and not the URL. The PostForm field supports only application/x-www-form-urlencoded.
+    Since we want the form key-value pairs, we can ignore the URL key-value pairs. The PostForm field provides key-value pairs only
+    for the form and not the URL. The PostForm field supports only application/x-www-form-urlencoded.
 
-  The FormValue method lets you access the key-value pairs just like the Form field, except that it's for a specific key and there
-  is no need to call the ParseForm method beforehand -- the FormValue method does it. The PostFormValue method does the same thing,
-  except that it's for the PostForm field instead of the Form field.
-  ***/
-  if ui := req.FormValue("compute"); ui != "" {  //Values from form and URL.
+    The FormValue method lets you access the key-value pairs just like the Form field, except that it's for a specific key and there
+    is no need to call the ParseForm method beforehand -- the FormValue method does it. The PostFormValue method does the same thing,
+    except that it's for the PostForm field instead of the Form field.
+    ***/
+  if ui := req.FormValue("compute"); ui != "" { //Values from form and URL.
     fields.CurrentPage = ui
   }
   //Dynamic variables determined by the routing route condition.
@@ -124,19 +123,19 @@ func (a WfAdEppPages) AdEppPages(res http.ResponseWriter, req *http.Request) {
     newSessionToken, newSession := sessions.UpdateEntryInSessions(sessionToken)
     http.SetCookie(res, sessions.CreateCookie(newSessionToken))
     partialTemplate = "n-i-FV.html"
-    templateData = struct{
-      LayoutType string
-      Header string
-      Datetime string
-      MenuPage string
+    templateData = struct {
+      LayoutType    string
+      Header        string
+      Datetime      string
+      MenuPage      string
       CurrentButton string
-      CsrfToken string
-      Fd1N string
+      CsrfToken     string
+      Fd1N          string
       Fd1TimePeriod string
-      Fd1Interest string
-      Fd1Compound string
-      Fd1FV string
-      Fd1Result string
+      Fd1Interest   string
+      Fd1Compound   string
+      Fd1FV         string
+      Fd1Result     string
     }{
       "standard",
       "Annuity Due / Equal Periodic Payments",
@@ -158,28 +157,33 @@ func (a WfAdEppPages) AdEppPages(res http.ResponseWriter, req *http.Request) {
     newSessionToken, newSession := sessions.UpdateEntryInSessions(sessionToken)
     http.SetCookie(res, sessions.CreateCookie(newSessionToken))
     partialTemplate = "n-i-PV.html"
-    templateData = struct{
-          LayoutType string
-          Header string
-          Datetime string
-          MenuPage string
-          CurrentButton string
-          CsrfToken string
-          Fd1N string
-          Fd1TimePeriod string
-          Fd1Interest string
-          Fd1Compound string
-          Fd1FV string
-          Fd1Result string
-        }{
-          "standard",
-          "Annuity Due / Equal Periodic Payments",
-          logger.DatetimeFormat(),
-          financesMenuPage,
-          fields.CurrentButton,
-          newSession.CsrfToken, fields.Fd1N, fields.Fd1TimePeriod, fields.Fd1Interest, fields.Fd1Compound, fields.Fd1FV, fields.Fd1Result },
-      })
-
+    templateData = struct {
+      LayoutType    string
+      Header        string
+      Datetime      string
+      MenuPage      string
+      CurrentButton string
+      CsrfToken     string
+      Fd1N          string
+      Fd1TimePeriod string
+      Fd1Interest   string
+      Fd1Compound   string
+      Fd1FV         string
+      Fd1Result     string
+    }{
+      "standard",
+      "Annuity Due / Equal Periodic Payments",
+      logger.DatetimeFormat(),
+      financesMenuPage,
+      fields.CurrentButton,
+      newSession.CsrfToken,
+      fields.Fd1N,
+      fields.Fd1TimePeriod,
+      fields.Fd1Interest,
+      fields.Fd1Compound,
+      fields.Fd1FV,
+      fields.Fd1Result,
+    }
   default:
     errString := fmt.Sprintf("Unsupported page: %s", fields.CurrentPage)
     logger.LogError(errString, correlationId)
@@ -189,108 +193,34 @@ func (a WfAdEppPages) AdEppPages(res http.ResponseWriter, req *http.Request) {
   templatesNeeded := []string{
     "webfinances/templates/layout.html",
     "webfinances/templates/finances/annuitydue/epp/epp.html",
-    "webfinances/templates/finances/annuitydue/epp/n-i-FV.html",
+    "webfinances/templates/finances/annuitydue/epp/" + partialTemplate,
     "webfinances/templates/title.html",
     "webfinances/templates/datetime.html",
     "webfinances/templates/navbar.html",
     "webfinances/templates/footer.html",
   }
-
-    //
-    if strings.EqualFold(fields.CurrentPage, "rhs-ui1") {
-      templatesNeeded := []string{
-        "webfinances/templates/layout.html",
-        "webfinances/templates/finances/annuitydue/epp/epp.html",
-        "webfinances/templates/finances/annuitydue/epp/n-i-FV.html",
-        "webfinances/templates/title.html",
-        "webfinances/templates/datetime.html",
-        "webfinances/templates/navbar.html",
-        "webfinances/templates/footer.html",
-      }
-      renderer.Render(res, "layout", templatesNeeded, renderer.PageData{
-        Data: struct{
-          LayoutType string
-          Header string
-          Datetime string
-          MenuPage string
-          CurrentButton string
-          CsrfToken string
-          Fd1N string
-          Fd1TimePeriod string
-          Fd1Interest string
-          Fd1Compound string
-          Fd1FV string
-          Fd1Result string
-        } { "standard", "Annuity Due / Equal Periodic Payments", logger.DatetimeFormat(), financesMenuPage, fields.CurrentButton,
-            newSession.CsrfToken, fields.Fd1N, fields.Fd1TimePeriod, fields.Fd1Interest, fields.Fd1Compound, fields.Fd1FV, fields.Fd1Result },
-      })
-    } else if strings.EqualFold(fields.CurrentPage, "rhs-ui2") {
-      fields.CurrentButton = "lhs-button2"
-      if req.Method == http.MethodPost {
-      }
-      newSessionToken, newSession := sessions.UpdateEntryInSessions(sessionToken)
-      cookie := sessions.CreateCookie(newSessionToken)
-      http.SetCookie(res, cookie)
-      templatesNeeded := []string{
-        "webfinances/templates/layout.html",
-        "webfinances/templates/finances/annuitydue/epp/epp.html",
-        "webfinances/templates/finances/annuitydue/epp/n-i-PV.html",
-        "webfinances/templates/title.html",
-        "webfinances/templates/datetime.html",
-        "webfinances/templates/navbar.html",
-        "webfinances/templates/footer.html",
-      }
-      renderer.Render(res, "layout", templatesNeeded, renderer.PageData{
-        Data: struct{
-          LayoutType string
-          Header string
-          Datetime string
-          MenuPage string
-          CurrentButton string
-          CsrfToken string
-          Fd2N string
-          Fd2TimePeriod string
-          Fd2Interest string
-          Fd2Compound string
-          Fd2PV string
-          Fd2Result string
-        } { "standard", "Annuity Due / Equal Periodic Payments", logger.DatetimeFormat(), financesMenuPage, fields.CurrentButton,
-            newSession.CsrfToken, fields.Fd2N, fields.Fd2TimePeriod, fields.Fd2Interest, fields.Fd2Compound, fields.Fd2PV, fields.Fd2Result },
-      })
-    } else {
-      errString := fmt.Sprintf("Unsupported page: %s", fields.CurrentPage)
-      logger.LogError(errString, correlationId)
-      panic(errString)
-    }
-    //
-    if req.Context().Err() == context.DeadlineExceeded {
-      logger.LogWarning("*** Request timeout ***", correlationId)
-      if strings.EqualFold(fields.CurrentPage, "rhs-ui1") {
-        fields.Fd1Result = ""
-      } else if strings.EqualFold(fields.CurrentPage, "rhs-ui2") {
-        fields.Fd2Result = ""
-      }
-    }
-    //
-    if data, err := json.Marshal(fields); err != nil {
-      logger.LogError(fmt.Sprintf("%+v", err), correlationId)
-    } else {
-      filePath := fmt.Sprintf("%s/%s/adepp.txt", mainDir, userName)
-      if _, err := osu.WriteAllExclusiveLock1(filePath, data, os.O_CREATE | os.O_RDWR | os.O_TRUNC, 0o600); err != nil {
-        logger.LogError(fmt.Sprintf("%+v", err), correlationId)
-      }
-    }
+  renderer.Render(res, "layout", templatesNeeded, renderer.PageData{Data: templateData})
+  data, err := json.Marshal(fields) //Preserve current choices.
+  if err != nil {
+    //Don't crash the server (panic), but log it clearly so you can debug the serialization.
+    logger.LogError(fmt.Sprintf("Failed to marshal fields to JSON for user %s: %+v", userName, err), correlationId)
   } else {
-    errString := fmt.Sprintf("Unsupported method: %s", req.Method)
-    logger.LogError(errString, correlationId)
-    panic(errString)
+    go func(userData []byte, uName, cId string) {
+      filePath := fmt.Sprintf("%s/%s/adepp.txt", mainDir, uName)
+      //The exclusive OS file lock handles goroutine collisions.
+      _, err := osu.WriteAllExclusiveLock1(filePath, userData, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0o600)
+      //Check the error returned from the lock-writing function.
+      if err != nil {
+        logger.LogError(fmt.Sprintf("Goroutine file system error writing state to %s: %+v", filePath, err), cId)
+        return
+      }
+      logger.LogInfo(fmt.Sprintf("Goroutine successfully persisted state to %s.", filePath), cId)
+    }(data, userName, correlationId) // Pass variables into the closure to prevent scope races
   }
   logger.LogInfo(fmt.Sprintf("Request took %vms\n", time.Since(startTime).Microseconds()), correlationId)
 }
 
-
-
-//Extraction helper for UI-1 calculations.
+// Extraction helper for UI-1 calculations.
 func (a WfAdEppPages) processUi1Form(req *http.Request, fields *adEppFields, correlationId string) {
   fields.Fd1N = req.PostFormValue("fd1-n")
   fields.Fd1TimePeriod = req.PostFormValue("fd1-tp")
@@ -309,14 +239,14 @@ func (a WfAdEppPages) processUi1Form(req *http.Request, fields *adEppFields, cor
     fields.Fd1Result = fmt.Sprintf("Error: %s -- %+v", fields.Fd1FV, err)
   } else {
     var oa finances.Annuities
-    fields.Fd1Result = fmt.Sprintf("Payment: $%.5f", oa.D_Payment_FV(fv, i / 100.0,
+    fields.Fd1Result = fmt.Sprintf("Payment: $%.5f", oa.D_Payment_FV(fv, i/100.0,
       oa.GetCompoundingPeriod(fields.Fd1Compound[0], true), n, oa.GetTimePeriod(fields.Fd1TimePeriod[0], true)))
   }
   logger.LogInfo(fmt.Sprintf("n = %s, tp = %s, i = %s, cp = %s, fv = %s, %s", fields.Fd1N,
     fields.Fd1TimePeriod, fields.Fd1Interest, fields.Fd1Compound, fields.Fd1FV, fields.Fd1Result), correlationId)
 }
 
-//Extraction helper for UI-2 calculations.
+// Extraction helper for UI-2 calculations.
 func (a WfAdEppPages) processUi2Form(req *http.Request, fields *adEppFields, correlationId string) {
   fields.Fd2N = req.PostFormValue("fd2-n")
   fields.Fd2TimePeriod = req.PostFormValue("fd2-tp")
@@ -335,7 +265,7 @@ func (a WfAdEppPages) processUi2Form(req *http.Request, fields *adEppFields, cor
     fields.Fd2Result = fmt.Sprintf("Error: %s -- %+v", fields.Fd2PV, err)
   } else {
     var oa finances.Annuities
-    fields.Fd2Result = fmt.Sprintf("Payment: $%.5f", oa.D_Payment_PV(pv, i / 100.0,
+    fields.Fd2Result = fmt.Sprintf("Payment: $%.5f", oa.D_Payment_PV(pv, i/100.0,
       oa.GetCompoundingPeriod(fields.Fd2Compound[0], true), n, oa.GetTimePeriod(fields.Fd2TimePeriod[0], true)))
   }
   logger.LogInfo(fmt.Sprintf("n = %s, tp = %s, i = %s, cp = %s, pv = %s, %s", fields.Fd2N,
